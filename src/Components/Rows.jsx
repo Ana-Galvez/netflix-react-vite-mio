4c5c2f23c96e01455df90e68435dacc959ee7bdc
ana galvez <130255411+Ana-Galvez@users.noreply.github.com>
2024-05-07 21:53:52 -0300

    agregado de flechas en las rows

--- src/Components/Rows.jsx
+++ src/Components/Rows.jsx
@@ -1,11 +1,20 @@
 import '../styles/Rows.css';
 import axios from '../features/axios';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 // eslint-disable-next-line react/prop-types
 const Rows = ({title, fetchUrl}) => {
-  const [movies, setMovies] = useState([]);
+  const [scrollPosition,setScrollPosition]=useState(0);
+  const rowScrollRef =useRef();
+
+  function handleScroll(scrollValue){
+    const newPosition =scrollPosition + scrollValue;
+    setScrollPosition(newPosition);
+    rowScrollRef.current.scrollLeft = newPosition;
+  }
+
+  const [movies, setMovies]   = useState([]);
   const urlBase = 'https://image.tmdb.org/t/p/original/';
 
   useEffect(() => {
@@ -17,18 +26,20 @@ const Rows = ({title, fetchUrl}) => {
     fetchData();
   }, [fetchUrl]);
   
-  console.log(movies);
+  // console.log(movies);
 
   return (
     <div className='rows'>
       <h2 className='rows__title'>{title}</h2>
-      <div className='rows__posters'>
+      <div className='rows__posters' ref={rowScrollRef}>
+        <img onClick={()=>{handleScroll(-400)}} className='rows__arrows left'  src="../../public/images/icons8-flecha-izquierda-48.png" alt="Flecha izquierda" />
         {movies.map(movie => (
           <Link to='/descripcionprueba' target="_blank" className='rows__poster' key={movie.id}>
-            <img className='rows__poster__img' key={movie.id} src={`${urlBase}${movie.poster_path}`} alt={movie.name} />
-            <p className='rows__movieName'>{(!movie.name?movie.title:movie.name)}</p>
+            <img className='rows__poster__img' src={`${urlBase}${movie.poster_path}`} alt={movie.name} />
+            {/* <p className='rows__movieName'>{(!movie.name?movie.title:movie.name)}</p> */}
           </Link>
         ))}
+        <img onClick={()=>{handleScroll(400)}} className='rows__arrows right' src="../../public/images/icons8-flecha-derecha-48.png" alt="Flecha derecha" />
       </div>
     </div>
   )
